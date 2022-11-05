8e70e8f75ef12170c020eb2d95a447ecc89366e9
wardds <4179769+wardds@users.noreply.github.com>
2022-11-05 12:52:15 +0200

    Disable ui scaling on background frame.
    This seems to fix the positioning problem for DS2 version for scales that are not 1

--- src/DarkSoulsDeathScreen.lua
+++ src/DarkSoulsDeathScreen.lua
@@ -150,7 +150,8 @@ local function GetBackground(version)
 
     local frame = background[version]
     if not frame then
-        frame = CreateFrame("Frame")
+        -- inherit from DefaultScaleFrame so UIScale does not affect sizing and positioning
+        frame = CreateFrame("Frame", nil,nil,"DefaultScaleFrame")
         frame.version = version
         background[version] = frame
 
