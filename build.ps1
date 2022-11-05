a9d175d7bf69c3de90cdeed35da9af261c0487a8
wardds <4179769+wardds@users.noreply.github.com>
2022-11-05 13:57:30 +0200

    Move files up a level.
    This way you can develop and use the addon straight from this folder

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 $Destination = "./dist/DarkSoulsDeathScreen";
 
+# Clean out the dist folder
 Remove-Item -Path ./dist -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path $Destination  -ItemType Directory -Force
-Copy-Item -Path ./src/* -Destination $Destination -Recurse
+
+# Copy all items to distribute to the target folder.
+# Remember to add more here if new folders/files are made.
+Copy-Item -Path ./DarkSoulsDeathScreen.* -Destination $Destination
+Copy-Item -Path ./media -Destination $Destination -Recurse
+
+# Zip it up
 Compress-Archive -Path $Destination -DestinationPath "$($Destination).zip"
-Remove-Item -Path $Destination -Recurse
